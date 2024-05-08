916bd18a47ffec04c4302a935cc8a2eea8c213b2
david van der Bent <davidvdbent@gmail.com>
2024-05-08 10:03:21 +0200

    Using Span for Abbr, and hovering on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,12 +7,12 @@ const Footer = () => {
         <div className="">
             <div className="flex justify-evenly mb-4">
                 <div className="p-3">
-                    <Link className='mb-4' to='/checkspage'><p className="mr-2 text-sm text-white font-thin font-montserrat">Toegewezen Rapportages</p></Link>
-                    <Link className='mt-4' to='/pastcheckpage'><p className="mr-2 mt-4 text-sm text-white font-thin font-montserrat">Verstuurde Rapportages</p></Link>
+                    <Link className='mb-4' to='/checkspage'><p className="mr-2 text-sm text-white font-thin font-montserrat hover:text-rec-blue">Toegewezen Rapportages</p></Link>
+                    <Link className='mt-4' to='/pastcheckpage'><p className="mr-2 mt-4 text-sm text-white font-thin font-montserrat hover:text-rec-blue">Verstuurde Rapportages</p></Link>
                 </div>
                 <div className="p-3">
-                    <Link className='mb-2' to='/documentation'><p className="mr-2 text-sm text-white font-thin font-montserrat">Kennisbank</p></Link>
-                    <Link className='' to='/settings'><p className="mr-2 mt-4 text-sm text-white font-thin font-montserrat">Instellingen</p></Link>
+                    <Link className='mb-2' to='/documentation'><p className="mr-2 text-sm text-white font-thin font-montserrat hover:text-rec-blue">Kennisbank</p></Link>
+                    <Link className='' to='/settings'><p className="mr-2 mt-4 text-sm text-white font-thin font-montserrat hover:text-rec-blue">Instellingen</p></Link>
                 </div>
             </div>
         </div>
